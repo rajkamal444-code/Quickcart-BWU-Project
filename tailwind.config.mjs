515c1f2b45df2de28a9dfb01f3442b90284ee860
rajkamal444-code <rajkamalshaw444@gmail.com>
2025-09-06 04:38:31 +0530

    Add files via upload

---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          300: '#8b94ff',
+          500: '#5f6fff',
+          600: '#5f6fff',
+          700: '#4a5aff',
+        }
+      },
+      gridTemplateColumns:{
+        'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+      },
+    },
+  },
+  plugins: [],
+};
